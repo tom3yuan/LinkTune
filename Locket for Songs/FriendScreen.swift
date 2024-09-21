9b8b27a9c0acc2b286505fb14f5c7fa708a1bcf1
tyytyyy <tom3yuan@gmail.com>
2024-09-21 15:39:17 -0500

    Allowed profile to show on profile page

---
+++ Locket for Songs/FriendScreen.swift
@@ -0,0 +1,32 @@
+//
+//  FriendScreen.swift
+//  Locket for Songs
+//
+//  Created by Tom Yuan on 9/21/24.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class FriendScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
