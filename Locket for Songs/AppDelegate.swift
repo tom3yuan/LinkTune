6bbec901f5a7c9430c907b470f6f360104111a34
tyytyyy <tom3yuan@gmail.com>
2024-09-21 16:30:21 -0500

    added features to add friend

--- Locket for Songs/AppDelegate.swift
+++ Locket for Songs/AppDelegate.swift
@@ -6,16 +6,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+            FirebaseApp.configure()
+            return true
+        }
+    
 
     // MARK: UISceneSession Lifecycle
 
--- Locket for Songs/FriendScreen.swift
+++ Locket for Songs/FriendScreen.swift
@@ -4,21 +4,43 @@
 //
 //  Created by Tom Yuan on 9/21/24.
 //
+struct User {
+    let username: String
+    }
 
 import UIKit
 import Firebase
 import FirebaseFirestore
 
 class FriendScreen: UIViewController {
-
+    @IBOutlet weak var friendTable: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    let db = Firestore.firestore()
+    var users: [User] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchUsernames()
         // Do any additional setup after loading the view.
     }
-    
+    func fetchUsernames() {
+        db.collection("users").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self.users = [] // Reset users array
+                for document in querySnapshot!.documents {
+                    if let username = document.data()["username"] as? String {
+                        self.users.append(User(username: username))
+                    }
+                }
+                self.updateUI() // Call function to update UI
+            }
+        }
+    }
+    func updateUI() {
+            friendTable.reloadData()
+        }
 
-    @IBOutlet weak var searchBar: UISearchBar!
     /*
     // MARK: - Navigation
 
@@ -29,4 +51,17 @@ class FriendScreen: UIViewController {
     }
     */
 
+}
+extension FriendScreen: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count // Return the number of users
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.username // Set the cell's text to the username
+        return cell
+    }
 }
