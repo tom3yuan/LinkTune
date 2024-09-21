07d10003fd45c35957aefdf84e1c9f5973c03e24
vt-tyuan <tyuan@veritone.com>
2024-09-20 23:50:51 -0500

    Initial Commit

---
+++ Locket for Songs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locket for Songs
+//
+//  Created by Tom Yuan on 9/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var startButton: UILabel!
+}
+
