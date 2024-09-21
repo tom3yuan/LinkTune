38fdf5535c397dac704b95af1129e4eb8f7ba5f8
vt-tyuan <tyuan@veritone.com>
2024-09-20 23:50:51 -0500

    Initial Commit

---
+++ Locket for SongsTests/Locket_for_SongsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Locket_for_SongsTests.swift
+//  Locket for SongsTests
+//
+//  Created by Tom Yuan on 9/20/24.
+//
+
+import Testing
+@testable import Locket_for_Songs
+
+struct Locket_for_SongsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Locket for SongsUITests/Locket_for_SongsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Locket_for_SongsUITestsLaunchTests.swift
+//  Locket for SongsUITests
+//
+//  Created by Tom Yuan on 9/20/24.
+//
+
+import XCTest
+
+final class Locket_for_SongsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
